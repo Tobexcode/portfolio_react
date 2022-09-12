81a9c73b6331b72a5d778b89e94b029b590a9e4b
tobexcode <tobexalox@gmail.com>
2022-09-12 13:28:19 +0100

    first commit3

--- src/container/Testimonial/Testimonial.jsx
+++ src/container/Testimonial/Testimonial.jsx
@@ -30,30 +30,30 @@ const Testimonial = () => {
 
   return (
     <>
-      {/* {testimonials.length && (
-        <>
-          <div className="app__testimonial-item app__flex">
-            <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
-            <div className="app__testimonial-content">
-              <p className="p-text">{testimonials[currentIndex].feedback}</p>
-              <div>
-                <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
-                <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+        {testimonials.length && (
+          <>
+            <div className="app__testimonial-item app__flex">
+              <img src={urlFor(testimonials[currentIndex].imgurl)} alt={testimonials[currentIndex].name} />
+              <div className="app__testimonial-content">
+                <p className="p-text">{testimonials[currentIndex].feedback}</p>
+                <div>
+                  <h4 className="bold-text">{testimonials[currentIndex].name}</h4>
+                  <h5 className="p-text">{testimonials[currentIndex].company}</h5>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="app__testimonial-btns app__flex">
-            <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
-              <HiChevronLeft />
-            </div>
+            <div className="app__testimonial-btns app__flex">
+              <div className="app__flex" onClick={() => handleClick(currentIndex === 0 ? testimonials.length - 1 : currentIndex - 1)}>
+                <HiChevronLeft />
+              </div>
 
-            <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
-              <HiChevronRight />
+              <div className="app__flex" onClick={() => handleClick(currentIndex === testimonials.length - 1 ? 0 : currentIndex + 1)}>
+                <HiChevronRight />
+              </div>
             </div>
-          </div>
-        </>
-      )} */}
+          </>
+        )}
       <div className="app__testimonial-brands app__flex">
         {brands.map((brand) => (
           <motion.div
