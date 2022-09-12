fb9044a6add99e9836f59b72ec525a4aba3531bf
tobexcode <tobexalox@gmail.com>
2022-09-12 11:46:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+// import { About } from './container/About';
+// import { Footer } from './container/Footer';
+// import { Header } from './container/Header';
+// import { Skills } from './container/Skills';
+// import { Testimonial } from './container/Testimonial';
+// import { Work } from './container/Work';
+
+import { Navbar } from './components';
+import './App.scss';
+
+const App = () => (
+  <div className="app">
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer />
+  </div>
+);
+
+export default App;
